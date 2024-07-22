fc5d5c5d26b3bfd9feca98974f9ac84d222cc0fd
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2024-07-22 20:28:29 +0200

    first commit with all the dependencies

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # django-rest-api-mini-project
+
+create an api to display our items from the database and add a new endpoint to add new items
---
+++ api/views.py
@@ -0,0 +1,20 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from base.models import Item
+from .serializers import ItemSerializer
+from api import serializers
+
+
+@api_view(["GET"])
+def get_data(request):
+    items = Item.objects.all()
+    serializer = ItemSerializer(items, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+def add_data(request):
+    serializer = ItemSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
